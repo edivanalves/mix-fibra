4795ce9a7e3d82011d69dd9071abdb0c4d8fc5cf
edivan alves <53585093+edivanalves@users.noreply.github.com>
2025-06-19 15:46:32 -0300

    vitae

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // Mude ESTA LINHA para apenas '/'
+  base: '/', // <-- Caminho do repositório
   plugins: [react()],
 })
